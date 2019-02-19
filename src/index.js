e37997f87ab917f61ac183dc06863a39e2a136a7
Ivan Kononovich <fogfug1@gmail.com>
2019-02-19 20:11:39 +0200

    feat: add function count love triangles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+module.exports = function getLoveTrianglesCount(arr) {
+  let count = 0;
+
+  arr.forEach((item, index) => {
+    if(arr[arr[item - 1] - 1] === index + 1) count++;
+  });  
+
+  return Math.floor(count / 3);
 };
+
